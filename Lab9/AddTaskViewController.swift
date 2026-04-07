337669a2472d58f33bad14ab62c1e1877812dceb
StevenCoverdale <S.coverdale@hotmail.com>
2026-04-07 19:07:12 -0400

    Implemented add-task screen with core data save

---
+++ Lab9/AddTaskViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddTaskViewController.swift
+//  Lab9
+//
+//  Created by steven coverdale on 2026-04-07.
+//
+
+import UIKit
+import CoreData
+
+class AddTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskTextField: UITextField!
+
+    private let context = (UIApplication.shared.delegate as! AppDelegate)
+        .persistentContainer.viewContext
+
+    @IBAction func saveTaskTapped(_ sender: UIButton) {
+        guard let text = taskTextField.text,
+              !text.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+
+        let task = Task(context: context)
+        task.title = text
+
+        do {
+            try context.save()
+            taskTextField.text = ""
+        } catch {
+            print("Failed to save task:", error)
+        }
+    }
+
+    @IBAction func viewTasksTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "showTasks", sender: nil)
+    }
+}
