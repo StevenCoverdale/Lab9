cc63139c40f8183a5743d1f10192ec2322eafd41
StevenCoverdale <S.coverdale@hotmail.com>
2026-04-07 19:16:31 -0400

    Updating Add Task View controller

--- Lab9/AddTaskViewController.swift
+++ Lab9/AddTaskViewController.swift
@@ -33,4 +33,35 @@ class AddTaskViewController: UIViewController {
     @IBAction func viewTasksTapped(_ sender: UIButton) {
         performSegue(withIdentifier: "showTasks", sender: nil)
     }
+    
+    private func editTask(at indexPath: IndexPath) {
+        let task = tasks[indexPath.row]
+
+        let alert = UIAlertController(title: "Edit Task",
+                                      message: nil,
+                                      preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.text = task.title
+        }
+
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let newText = alert.textFields?.first?.text,
+                  !newText.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+
+            task.title = newText
+            do {
+                try self.context.save()
+                self.tableView.reloadRows(at: [indexPath], with: .automatic)
+            } catch {
+                print("Failed to update task:", error)
+            }
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+
 }
