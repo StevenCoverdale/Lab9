041259e4cf38d834d5bec5c187c56ad75fd6a20f
StevenCoverdale <S.coverdale@hotmail.com>
2026-04-07 19:17:42 -0400

    Simplifying AddTaskViewController

--- Lab9/AddTaskViewController.swift
+++ Lab9/AddTaskViewController.swift
@@ -15,6 +15,11 @@ class AddTaskViewController: UIViewController {
     private let context = (UIApplication.shared.delegate as! AppDelegate)
         .persistentContainer.viewContext
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Add Task"
+    }
+
     @IBAction func saveTaskTapped(_ sender: UIButton) {
         guard let text = taskTextField.text,
               !text.trimmingCharacters(in: .whitespaces).isEmpty else { return }
@@ -33,35 +38,4 @@ class AddTaskViewController: UIViewController {
     @IBAction func viewTasksTapped(_ sender: UIButton) {
         performSegue(withIdentifier: "showTasks", sender: nil)
     }
-    
-    private func editTask(at indexPath: IndexPath) {
-        let task = tasks[indexPath.row]
-
-        let alert = UIAlertController(title: "Edit Task",
-                                      message: nil,
-                                      preferredStyle: .alert)
-        alert.addTextField { textField in
-            textField.text = task.title
-        }
-
-        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
-            guard let newText = alert.textFields?.first?.text,
-                  !newText.trimmingCharacters(in: .whitespaces).isEmpty else { return }
-
-            task.title = newText
-            do {
-                try self.context.save()
-                self.tableView.reloadRows(at: [indexPath], with: .automatic)
-            } catch {
-                print("Failed to update task:", error)
-            }
-        }
-
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-
-        alert.addAction(saveAction)
-        alert.addAction(cancelAction)
-        present(alert, animated: true)
-    }
-
 }
